f6366f3ca0ae38f78a816517f13d472dd743f4b7
Kawan Santana <kawan.santana@estudante.ifms.edu.br>
2020-05-18 17:23:55 -0300

    Logo e header

--- site/resources/views/layout/header.blade.php
+++ site/resources/views/layout/header.blade.php
@@ -7,9 +7,9 @@
     <title>{{config('app.name')}}</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-light" style="padding:0">
-        <div class="container bg-light">
-          <a class="navbar-brand" style="background-color: #802B26; padding:20px" href="#"><img src="img/logo.png" height="50px"></a>
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" style="padding:0">
+        <div class="container">
+          <a class="navbar-brand" href="#"><img src="img/logo_final_final.png" height="140vh"></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
@@ -18,7 +18,7 @@
               <li class="nav-item active">
                 <a class="nav-link" href="#">Home
                       <span class="sr-only">(current)</span>
-                    </a>
+                  </a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">About</a>
